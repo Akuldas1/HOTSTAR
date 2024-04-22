81183dad5c7ee462c86e6d2eb428ada0de6872e0
Akuldas1 <akuldas651@gmail.com>
2024-04-22 07:20:36 +0530

    initial commit

---
+++ Hotstar_clone/tailwind.config.js
@@ -0,0 +1,33 @@
+// /** @type {import('tailwindcss').Config} */
+// export default {
+//   content: [],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// }
+
+// /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//   content: ["./src/**/*.{html,js}"],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// }
+
+export default {
+  content:[
+    "./index.html",
+    "./src/**/*.{jsx,js}"],
+  theme: {
+    extend: {},
+    screens: {
+      "desktop": "1800px",
+      "laptops": "1500px",
+      "tablets": "900px",
+      "mobiles": "400px"
+    }
+  },
+  plugins: [],
+}
