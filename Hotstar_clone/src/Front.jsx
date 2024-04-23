58eb4e097edd84664eb553f31c55fdd202de8e65
Akuldas1 <akuldas651@gmail.com>
2024-04-23 17:38:16 +0530

    change

--- Hotstar_clone/src/Front.jsx
+++ Hotstar_clone/src/Front.jsx
@@ -3,7 +3,14 @@ function Front() {
   return (
     <>
       <div className='video'>
-        <video src={video} autoPlay loop muted></video>
+        <ul className='text-white flex gap-4 absolute left-28 top-1/3 font-bold'>
+          <li>Malayalam</li>
+          <li>2024</li>
+          <li>U/A 16+</li>
+        </ul>
+        <p className='text-white absolute left-28 top-64 font-bold w-1/3'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur unde eligendi facilis tenetur illo voluptate distinctio qui laborum ratione repudiandae, aperiam omnis! Voluptatibus aliquam laborum neque eveniet ullam consequuntur aspernatur.</p>
+        <p className='text-white absolute left-28 top-96 font-bold'>Thriller | Crime | Revenge</p>
+        <video src={video} autoPlay loop muted />
       </div>
     </>
   )
